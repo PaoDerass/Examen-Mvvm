a91bcce76bff3372e3f361e096a7bf968873829c
PaoDerass <paola_deras@unitec.edu>
2024-09-16 22:06:48 -0600

    agregar excepciones

--- Viewmodels/MainPageViewModel.cs
+++ Viewmodels/MainPageViewModel.cs
@@ -32,36 +32,49 @@ namespace ExamenMvvm
             Pro3 = 0;
         }
 
-        
+
         [RelayCommand]
         public void Calcular()
         {
-            
-            Subtotal = Pro1 + Pro2 + Pro3;
+            try
+            {
+             
+                if (Pro1 < 0 || Pro2 < 0 || Pro3 < 0)
+                {
+                    throw new ArgumentException("Los valores de los productos no pueden ser negativos.");
+                }
 
-            
-            if (Subtotal >= 10000)
-            {
-                Descuento = Subtotal * 0.30m; 
-            }
-            else if (Subtotal >= 5000)
-            {
-                Descuento = Subtotal * 0.20m; 
-            }
-            else if (Subtotal >= 1000)
-            {
-                Descuento = Subtotal * 0.10m; 
-            }
-            else
-            {
-                Descuento = 0; 
-            }
+                
+                Subtotal = Pro1 + Pro2 + Pro3;
 
-           
-            TotalPagar = Subtotal - Descuento;
+                
+                if (Subtotal >= 10000)
+                {
+                    Descuento = Subtotal * 0.30m; 
+                }
+                else if (Subtotal >= 5000)
+                {
+                    Descuento = Subtotal * 0.20m; 
+                }
+                else if (Subtotal >= 1000)
+                {
+                    Descuento = Subtotal * 0.10m; 
+                }
+                else
+                {
+                    Descuento = 0; 
+                }
+
+                
+                TotalPagar = Subtotal - Descuento;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error al calcular: {ex.Message}");
+            }
         }
 
-  
+
         [RelayCommand]
         public void Limpiar()
         {
